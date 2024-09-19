8a047d5dd6560fd969db00796c85b0fcb65761b8
khumeshdahare <130744834+khumeshdahare@users.noreply.github.com>
2024-09-19 13:21:44 +0530

    sever express setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app= express()
+
+const port= 3000
+
+
+app.use(express.json())
+
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+app.listen(port,()=>{
+    console.log("app is rnnign")
+})
